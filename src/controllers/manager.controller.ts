4ef5e18b597d7ebf64321f6cd7e48656c327625b
ihorryba <rybaihor@gmail.com>
2021-10-23 13:05:30 +0300

    "first commit"

--- src/controllers/manager.controller.ts
+++ src/controllers/manager.controller.ts
@@ -1,10 +1,29 @@
+class Person {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  getName(): string {
+    return this.name;
+  }
+}
+
+class User extends Person {
+  constructor(name: string) {
+    super(name);
+  }
+}
+
+let user: User = new User('Ivan');
+
 import {get} from '@loopback/rest';
 
 export class ManagerController {
-  constructor() {}
+  constructor() { }
 
   @get('/user')
-  user(): any {
-    return 'Hello World!';
+  user(): string {
+
+    return "Hello World!";
   }
 }
---
+++ src/controllers/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  firstName: string;
+  lastName: string;
+
+  constructor(name: string, surname: string) {
+    this.firstName = name;
+    this.lastName = surname;
+  }
+
+  getFullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
+}
